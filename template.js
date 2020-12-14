27feaebca4e75d8c4cc088f7fccc44dfd732593c
Guillermo Gonzalez <ggonmar@gmail.com>
2020-12-14 15:00:27 +0100

    New approach

--- template.js
+++ template.js
@@ -1,7 +1,21 @@
 try{input = document.getElementsByTagName("pre")[0].innerText;}
 catch(e){
-    input=``;
+    input=getLiteralInput();
 }
 input = input.split("\n").filter(e => e.length);
 
 
+function dayX(input){
+
+}
+
+function dayX_2(input){
+
+}
+
+dayX(input);
+dayX_2(input);
+
+function getLiteralInput(){
+    return ``;
+}
